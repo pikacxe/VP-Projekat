dfb47f656495d6ac03fd311e32bbd66da75f6927
Ilija Spasic <spasicilija1@gmail.com>
2023-05-10 18:57:18 +0200

    Added Common clases. Started UI implementation

---
+++ Common/IFileHandling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IFileHandling
+    {
+        [OperationContract]
+        ReceivedFileOptions SendXML(SendFileOptions file);
+
+    }
+}
--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
+using System.ServiceModel;
+using System.IO;
+using System.Configuration;
+using System.Xml;
 
 namespace UserInterface
 {
@@ -10,6 +15,20 @@ namespace UserInterface
     {
         static void Main(string[] args)
         {
+            var xmlPath = ConfigurationManager.AppSettings["xmlPath"];
+            while (true)
+            {
+                if (File.Exists(xmlPath))
+                {
+                    Console.WriteLine($"Cekam kreiranje ili modifikovanja fajla: {xmlPath}");
+                    while (!File.Exists(xmlPath)) ;
+                }
+                FileInfo fi = new FileInfo(xmlPath);
+                DateTime fileCreated = fi.CreationTime;
+                FileStream f = File.Open(xmlPath, FileMode.Open);
+            }
+
         }
+
     }
 }
